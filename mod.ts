0059c582ffa7e2f3c6a306cbafee5b57e54c13c0
Joshua Byrd <phocks@gmail.com>
2025-11-06 06:10:51 +0000

    OK this might work

--- mod.ts
+++ mod.ts
@@ -1,3 +1,11 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export function emitResize(height: number) {
+  if (!window.parent) {
+    return;
+  }
+  const payload = {
+    type: "embed-size",
+    height,
+  };
+
+  window.parent.postMessage(payload, "*");
 }
