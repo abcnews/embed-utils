23a3a160a5fe17ffacefe75951b4ae5d5e5b5971
Joshua Byrd <phocks@gmail.com>
2025-11-07 02:49:42 +0000

    emitResize

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,17 @@
-export const add = (a: number, b: number) => {
-  return a + b;
-}
+/**
+ * Emits a message to the parent window to resize the iframe
+ * @param height
+ * @returns void
+ */
+export const emitResize = (height: number) => {
+  if (!globalThis.parent) {
+    return;
+  }
+
+  const payload = {
+    type: "embed-size",
+    height,
+  };
+
+  globalThis.parent.postMessage(payload, "*");
+};
