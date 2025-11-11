3fe84ae8e909109fedf0ca9ba99b256cc6a2ddcf
Joshua Byrd <phocks@gmail.com>
2025-11-11 13:18:17 +1000

    React

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,28 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import dts from "unplugin-dts/vite";
+import react from "@vitejs/plugin-react";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: {
+        // Core functions (vanilla JS)
+        index: resolve(__dirname, "lib/core/index.ts"),
+        // React wrapper
+        // react: resolve(__dirname, "lib/react/index.tsx"),
+        // Svelte wrapper
+        // svelte: resolve(__dirname, "lib/svelte/index.ts"),
+      },
       name: "embed-utils",
       fileName: "embed-utils",
     },
   },
-  plugins: [
-    dts({ rollupTypes: true }),
-  ],
-  // rollupOptions: {
+  plugins: [react(), dts({ bundleTypes: true })],
+  // rollupOptions:   {
   // make sure to externalize deps that shouldn't be bundled
   // into your library
   // external: ['vue'],
